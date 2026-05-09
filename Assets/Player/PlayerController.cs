ef7a3fff839393bd4b80f4e69e4a5ad0f8494dcc
KentGab27 <155821672+KentGab27@users.noreply.github.com>
2026-05-09 18:37:35 +0800

    PlayerControls
    Player movement left and right

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+
+    private Rigidbody2D rb;
+    private PlayerInputActions inputActions;
+    private InputAction moveAction;
+    private float moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        inputActions = new PlayerInputActions();
+        moveAction = inputActions.Player.Move;
+    }
+    
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<float>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveInput * moveSpeed;
+
+        rb.linearVelocity = velocity;
+    }
+}
